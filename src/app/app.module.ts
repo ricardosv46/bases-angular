78b39064f41ce440c646dfae20df5a18996e2b96
ricardo <ricardosv46@gmail.com>
2023-07-22 18:53:50 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    CounterModule,
+    HeroesModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
